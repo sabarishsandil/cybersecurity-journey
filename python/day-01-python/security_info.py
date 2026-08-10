c729112bdd4cab7b96df8a70de5711bc35be081b
sabarishsandil <sabarish.sandilyan12@gmail.com>
2026-08-10 11:02:08 +0000

    Day 1: Add Python mini project

---
+++ python/day-01-python/security_info.py
@@ -0,0 +1,10 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+username = input("Enter your username: ")
+
+print("\n--- User Information ---")
+print("Name:", name)
+print("Age:", age)
+print("Username:", username)
+print("Username length:", len(username))
+
